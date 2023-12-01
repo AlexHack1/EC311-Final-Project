e45a7ac703b49f775d015b8291dc545aa51dd6f8
proconlon <jamesconlon52@gmail.com>
2023-12-01 13:52:41 -0500

    systemverilog function generator

---
+++ final.srcs/sim_1/new/pwm_tb.v
@@ -0,0 +1,20 @@
+
+module tb;
+
+    // Inputs
+    reg Clk;
+
+    // Outputs
+    wire [7:0] data_out;
+
+    // Instantiate the Unit Under Test (UUT)
+    sine_wave_gen uut (
+        .Clk(Clk), 
+        .data_out(data_out)
+    );
+
+    //Generate a clock with 10 ns clock period.
+    initial Clk = 0;
+    always #5 Clk = ~Clk;
+    
+endmodule
