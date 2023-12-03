71188de7526a813006510ee36a7df55c916638ef
proconlon <jamesconlon52@gmail.com>
2023-12-02 22:13:59 -0500

    pwm basic waveform

--- final.srcs/sources_1/new/PWM.v
+++ final.srcs/sources_1/new/PWM.v
@@ -1,18 +1,21 @@
-`timescale 1ns / 1ps
+module pwm_generator (
+    input clk,
+    input reset,
+    input [7:0] duty_cycle, // 8-bit duty cycle  (0-255)
+    output reg pwm_signal
+);
 
-module PWM_generator(
-    input clk,  
-    input [15:0] sinu_sum, 
-    output logic pwm = 0
-);     
-logic [15:0] count = 0;    
-always_ff@(posedge clk)
-begin
-    count <= count + 1;
-    if(count <= sinu_sum)
-        pwm <= 1;
-    else
-        pwm <= 0;    
+// 8 bit counter
+reg [7:0] counter = 0;
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        counter <= 0;
+        pwm_signal <= 0;
+    end else begin
+        counter <= counter + 1'b1;
+        pwm_signal <= (counter < duty_cycle) ? 1'b1 : 1'b0; // PWM signal is high when counter is less than duty cycle
+    end
 end
 
 endmodule
